9de6d6395d25615f7dbc0439f6b055919aa6d3c4
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2013-06-21 20:32:54 -0400

    .

--- interface/qe.py
+++ interface/qe.py
@@ -1,12 +1,12 @@
 # striped interface for QE
 import re
-import fileinput
 import numpy as np
 from modulation.units import Bohr
+from structure.atoms import Atoms
 
 def read_qe(filename, symbols=None):
     alat = 0.0
-
+    cell = []
     output = open(filename)
     while True:
         line = output.readline()
@@ -21,7 +21,6 @@ def read_qe(filename, symbols=None):
 
         p = re.search(r'crystal axes: \(cart\. coord\. in units of alat\)', line)
         if p:
-            cell = []
             for i in range(0, 3):
                 p1 = re.search(r'.+?(-*\d+\.\d+)\s+(-*\d+\.\d+)\s+(-*\d+\.\d+)', output.readline())
                 cell.append([ float(p1.group(1)), float(p1.group(2)), float(p1.group(3)) ])
@@ -46,59 +45,5 @@ def read_qe(filename, symbols=None):
                 expanded_symbols.append( p1.group(1).upper() )
                 positions.append([ float(p1.group(2)), float(p1.group(3)), float(p1.group(4)) ])
 
-            print positions
-            print expanded_symbols
-            #atoms = Atoms(symbols=expaned_symbols,
-            #          cell=cell,
-            #          scaled_positions=positions)
-
-
-
-def get_atoms_from_output(f, symbols):
-    lines = f.readlines()
-
-    line1 = [x for x in lines[0].split()]
-    if is_exist_symbols(line1):
-        symbols = line1
-
-    scale = float(lines[1])
-
-    cell = []
-    for i in range(2, 5):
-        cell.append([float(x) for x in lines[i].split()[:3]])
-    cell = np.array(cell) * scale
-
-    try:
-        num_atoms = np.array([int(x) for x in lines[5].split()])
-        line_at = 6
-    except ValueError:
-        symbols = [x for x in lines[5].split()]
-        num_atoms = np.array([int(x) for x in lines[6].split()])
-        line_at = 7
-    
-    expaned_symbols = expand_symbols(num_atoms, symbols)
-
-    if lines[line_at][0].lower() == 's':
-        line_at += 1
-
-    is_scaled = True
-    if (lines[line_at][0].lower() == 'c' or
-        lines[line_at][0].lower() == 'k'):
-        is_scaled = False
-
-    line_at += 1
-
-    positions = []
-    for i in range(line_at, line_at + num_atoms.sum()):
-        positions.append([float(x) for x in lines[i].split()[:3]])
-
-    if is_scaled:
-        atoms = Atoms(symbols=expaned_symbols,
-                      cell=cell,
-                      scaled_positions=positions)
-    else:
-        atoms = Atoms(symbols=expaned_symbols,
-                      cell=cell,
-                      positions=positions)
-        
-    return atoms
+            atoms = Atoms(symbols=expanded_symbols, cell=cell, positions=positions)
+            print atoms
---
+++ test.py
@@ -0,0 +1,6 @@
+from interface.qe import read_qe
+from structure.atoms import Atoms
+
+
+read_qe("scf.out")
+
